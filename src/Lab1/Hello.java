10106c04e7f82915b551ccf93f2cc9b80fd05d61
samPuriwat <sam.puriwat@gmail.com>
2019-11-28 11:15:29 +0700

    add message

--- src/Lab1/Hello.java
+++ src/Lab1/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World, I'm SAM");
+        System.out.println("Information System");
 
 
     }//main
